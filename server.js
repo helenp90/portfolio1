8fb5b898e2a336ad3b040bd8fe1184187f2fbb7c
Helen Pitt <helenpitt1990@gmail.com>
2018-03-28 20:01:15 -0400

    declared the port

--- server.js
+++ server.js
@@ -2,27 +2,26 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+
+var port = process.env.PORT || 3000;
+
 var app = express();
 
-//Middleware
+//Middleware===========================>
 
-// var logger = function(req, res, next){
-//     console.log('logging....')
-//     next();
-// }
-// app.use(logger);
-//MW for bodyparser
+//bodyparser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-//MW for views
+//views
 app.use(express.static(path.join(__dirname,'public')));
 
-
+//rendering the html page
 
 app.get('/', function(req, res){
     res.render('./public/index');
 });
+//=============>End Middleware
 
 app.listen(3000, function(){
     console.log('Server starting on Port 3000');
